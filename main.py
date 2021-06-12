a3045b0e7764b9b94fb7b6802190e95cc6dce5a5
451 <75197463+451EK@users.noreply.github.com>
2021-06-12 09:04:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ from discord.ext import commands
 import os
 
 intents = discord.Intents().all()
-Bot = commands.Bot(command_prefix="-",intents=intents)
+Bot = commands.Bot(command_prefix="-",intents=intents,help_command=None)
 TOKEN = open('token.txt','r').read()
 
 @Bot.event
 async def on_ready():
-    await Bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="-chelp | Version 0.0.1"))
+    await Bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="-help | Version 0.0.1"))
     print("Bot is online!")
 
 for filename in os.listdir("./Commands"):
