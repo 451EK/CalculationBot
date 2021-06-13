ea4fdf918326c80cc2c187cd0a82bf74a360b859
451 <75197463+451EK@users.noreply.github.com>
2021-06-13 17:00:58 +0300

    Update ping.py

--- Commands/ping.py
+++ Commands/ping.py
@@ -12,11 +12,11 @@ class Ping(commands.Cog):
             REDembed=discord.Embed(title="**Pong! 🏓**",description=f"**Ping {round(self.Bot.latency * 1000)} ms**",colour=discord.Color.red(),timestamp=datetime.datetime.utcnow())
             REDembed.set_footer(text="Calculation#1494",icon_url=self.Bot.user.avatar_url)
             await ctx.send(embed=REDembed)
-        if round(self.Bot.latency * 1000) < 100:
+        elif round(self.Bot.latency * 1000) < 100:
             ORANGEembed=discord.Embed(title="**Pong! 🏓**",description=f"**Ping {round(self.Bot.latency * 1000)} ms**",colour=discord.Color.orange(),timestamp=datetime.datetime.utcnow())
             ORANGEembed.set_footer(text="Calculation#1494",icon_url=self.Bot.user.avatar_url)
             await ctx.send(embed=ORANGEembed)
-        if round(self.Bot.latency * 1000) < 50:
+        elif round(self.Bot.latency * 1000) < 50:
             GREENembed=discord.Embed(title="**Pong! 🏓**",description=f"**Ping {round(self.Bot.latency * 1000)} ms**",colour=discord.Color.green(),timestamp=datetime.datetime.utcnow())
             GREENembed.set_footer(text="Calculation#1494",icon_url=self.Bot.user.avatar_url)
             await ctx.send(embed=GREENembed)
