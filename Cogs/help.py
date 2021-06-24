5b1a8af25fc3a8b0a7b7c3ab0924bcb5d0567ff8
451 <cilmanemirkaan@gmail.com>
2021-06-24 19:36:01 +0300

    ⚡ Help Command

--- Cogs/help.py
+++ Cogs/help.py
@@ -8,7 +8,7 @@ class Help(commands.Cog):
     @commands.command()
     async def help(self,ctx):
         embed=discord.Embed(title="**Commands**",description="- **setprefix** <prefix> : _changes the prefix_\n- **add** <number1> <number2> : _adds up the numbers_\n- **sub** <number1> <number2> : _subtracts the numbers_\n- **mul** <number1> <number2> : _multiplies the numbers_\n- **div** <number1> <number2> : _divides the numbers_\n- **credits** : _shows the credits_\n- **help** : _shows the help message_\n- **ping** : _shows the bot latency_",colour=discord.Color.green())
-        await ctx.send(embed=embed
+        await ctx.send(embed=embed)
 
 def setup(Bot):
     Bot.add_cog(Help(Bot))
