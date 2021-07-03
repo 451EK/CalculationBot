29f14b59da276593d3b77e0e74ff69fdbf26537e
451 <75197463+451EK@users.noreply.github.com>
2021-07-03 11:42:35 +0300

    ⚡ Factorial Command

---
+++ Cogs/factorial.py
@@ -0,0 +1,28 @@
+import discord
+from discord.ext import commands
+import math
+
+class Factorial(commands.Cog):
+    def __init__(self,Bot):
+        self.Bot = Bot
+
+    @commands.command()
+    async def factorial(self,ctx,number = None):
+        try:
+            number = int(number)
+            if number is None:
+                WARNembed=discord.Embed(title="**Error**",description=f"• **{ctx.message.author.mention},make sure you typed the command correctly!**\n• **Usage :** `-factorial <number>`",colour=discord.Color.red())
+                await ctx.send(embed=WARNembed,delete_after=10)
+                await ctx.message.delete()
+                return
+            else:
+                result = math.factorial(number)
+                RESULTembed = discord.Embed(title="**Result**",description=f"{number}! = {result}")
+                await ctx.send(embed=RESULTembed)
+        except ValueError:
+            ERRORembed = discord.Embed(title="**Error**",description=f"• **{ctx.message.author.mention},you can't use decimals with factorial command.**",colour=discord.Color.red())
+            await ctx.send(embed=ERRORembed,delete_after=10)
+            await ctx.message.delete()
+
+def setup(Bot):
+    Bot.add_cog(Factorial(Bot))
