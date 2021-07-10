b20eaed5f2323c32054f2699cd89a531ab85c3df
451 <75197463+451EK@users.noreply.github.com>
2021-07-10 18:45:06 +0300

    ⚡ Factorial Command

--- Cogs/factorial.py
+++ Cogs/factorial.py
@@ -16,9 +16,14 @@ class Factorial(commands.Cog):
                 await ctx.message.delete()
                 return
             else:
-                result = math.factorial(number)
-                RESULTembed = discord.Embed(title="**Result**",description=f"{number}! = {result}")
-                await ctx.send(embed=RESULTembed)
+                if len(str(number)) > 3:
+                    WARN = discord.Embed(title="**Error**",description=f"• **{ctx.message.author.mention},length of entered number must be less than four!**",colour=discord.Color.red())
+                    await ctx.send(embed=WARN,delete_after=8)
+                    await ctx.message.delete()
+                else:
+                    result = math.factorial(number)
+                    RESULTembed = discord.Embed(title="**Result**",description=f"{number}! = {result}")
+                    await ctx.send(embed=RESULTembed)
         except ValueError:
             ERRORembed = discord.Embed(title="**Error**",description=f"• **{ctx.message.author.mention},you can't use decimals with factorial command.**",colour=discord.Color.red())
             await ctx.send(embed=ERRORembed,delete_after=10)
