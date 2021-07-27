00aca903ac90a457e62e2e1d0af480611f6ee873
Emir Kaan <cilmanemirkaan@gmail.com>
2021-07-27 16:52:41 +0300

    ⚡ Ping Command

--- Cogs/ping.py
+++ Cogs/ping.py
@@ -10,15 +10,15 @@ class Ping(commands.Cog):
     async def ping(self,ctx):
         if round(self.Bot.latency * 1000) > 100:
             REDembed=discord.Embed(title="**Pong! 🏓**",description=f"**Ping {round(self.Bot.latency * 1000)} ms**",colour=discord.Color.red(),timestamp=datetime.datetime.utcnow())
-            REDembed.set_footer(text="Calculation#1494",icon_url=self.Bot.user.avatar_url)
+            REDembed.set_footer(text="Calculator#0261",icon_url=self.Bot.user.avatar_url)
             await ctx.send(embed=REDembed)
         elif round(self.Bot.latency * 1000) < 100:
             ORANGEembed=discord.Embed(title="**Pong! 🏓**",description=f"**Ping {round(self.Bot.latency * 1000)} ms**",colour=discord.Color.orange(),timestamp=datetime.datetime.utcnow())
-            ORANGEembed.set_footer(text="Calculation#1494",icon_url=self.Bot.user.avatar_url)
+            ORANGEembed.set_footer(text="Calculator#0261",icon_url=self.Bot.user.avatar_url)
             await ctx.send(embed=ORANGEembed)
         elif round(self.Bot.latency * 1000) < 50:
             GREENembed=discord.Embed(title="**Pong! 🏓**",description=f"**Ping {round(self.Bot.latency * 1000)} ms**",colour=discord.Color.green(),timestamp=datetime.datetime.utcnow())
-            GREENembed.set_footer(text="Calculation#1494",icon_url=self.Bot.user.avatar_url)
+            GREENembed.set_footer(text="Calculator#0261",icon_url=self.Bot.user.avatar_url)
             await ctx.send(embed=GREENembed)
     
 def setup(Bot):
