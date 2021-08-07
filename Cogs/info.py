bb60e9e0f5891a967f12ca816377a43c5cc0c7ff
Emir Kaan <cilmanemirkaan@gmail.com>
2021-08-07 19:41:41 +0300

    ⚡ Info Command

--- Cogs/info.py
+++ Cogs/info.py
@@ -12,6 +12,7 @@ class Info(commands.Cog):
 
     @commands.command()
     async def info(self,ctx):
+        total_members = sum([len(guild.members) for guild in self.Bot.guilds])
         delta_uptime = datetime.utcnow() - self.Bot.launch_time
         hours, remainder = divmod(int(delta_uptime.total_seconds()), 3600)
         minutes, seconds = divmod(remainder, 60)
@@ -20,10 +21,10 @@ class Info(commands.Cog):
         embed.set_footer(text=f"Requested By {ctx.message.author}",icon_url=ctx.message.author.avatar_url)
         embed.set_thumbnail(url=self.Bot.user.avatar_url)
         embed.add_field(name="**Author**",value="451#2950")
-        embed.add_field(name="Communication",value="[Server](https://discord.gg/3BB8XS27Pw) & [Developer](https://discordapp.com/users/453613270725558292)")
+        embed.add_field(name="Communication",value="[Support Server](https://discord.gg/tXd9gPtKCj) & [Developer](https://discordapp.com/users/453613270725558292)")
         embed.add_field(name="Uptime",value=f"{days}d, {hours}h, {minutes}m")
         embed.add_field(name="Servers",value=len(self.Bot.guilds))
-        embed.add_field(name="Users",value=ctx.guild.member_count)
+        embed.add_field(name="Users",value=total_members)
         embed.add_field(name="Ping",value=f"{round(self.Bot.latency * 1000)}ms")
         embed.add_field(name="Links",value="[Github Repository](https://github.com/451EK/Calculator) | [Invite Link](https://discord.com/oauth2/authorize?client_id=869500014899122246&scope=bot&permissions=67488833)",inline=False)
         embed.timestamp = datetime.utcnow()
