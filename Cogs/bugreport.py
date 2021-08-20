05531d00244e0d57f2fe1d7b84458303aefdba16
451EK <cilmanemirkaan@gmail.com>
2021-08-20 20:17:09 +0300

    ⚡ Bugreport Command

--- Cogs/bugreport.py
+++ Cogs/bugreport.py
@@ -14,7 +14,7 @@ class BugReport(commands.Cog):
             await ctx.message.delete()
             return
         else:
-            channel = self.Bot.get_channel(870009763453345832)
+            channel = self.Bot.get_channel(872581407413764136)
             embed = discord.Embed(title="New Bug Report",description=f"🚨 {message}",colour=discord.Color.from_rgb(0,255,148),timestamp=datetime.datetime.utcnow())
             embed.set_footer(text=f"Sent by {ctx.message.author}",icon_url=ctx.message.author.avatar_url)
             scembed = discord.Embed(title="Your Report Has Successfully Been Sent!",colour=discord.Color.from_rgb(0,255,148))
