1425013ea9d68ca390f71eb990a9b91b2ca878dd
451 <cilmanemirkaan@gmail.com>
2021-09-21 21:33:21 +0300

    Bot adds wave emoji if it's mentioned

--- Events/botMention.js
+++ Events/botMention.js
@@ -1,10 +1,7 @@
 const client = require("../app.js")
-defaultPrefix = "`-`"
 
 client.on("messageCreate", message => {
-    if (message.content.includes("@here") || message.content.includes("@everyone")) return false;
     if (message.mentions.has(client.user.id)) {
-        message.reply(`Hello, my prefix is **${defaultPrefix}**(dash).\nUse the help command to see all commands!`);
         message.react("👋")
     };
 })
