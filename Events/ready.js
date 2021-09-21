efdf3efb44e6521f250faedaabf0e26b0339f4e6
451EK <cilmanemirkaan@gmail.com>
2021-09-21 21:28:31 +0300

    Added statuses

--- Events/ready.js
+++ Events/ready.js
@@ -5,6 +5,18 @@ client.on('ready', async client =>{
     client.guilds.cache.forEach(guilds => allUsers.push(guilds.memberCount))
     let users = allUsers.reduce((a, b) => a + b)
     console.log(`Logged in as ${client.user.tag} on ${client.guilds.cache.size} servers!`)
-
-    client.user.setPresence({status: "online", activities:[{name: `-help | ${users} Users`, type:"WATCHING"}]}) 
+    let statuses = [
+        `👨‍🏫 ${users} Users`,
+        `⚒️ -help`
+    ]
+    let i = 0;
+    setInterval(() => {
+        let status = statuses[i];
+        if(!status){
+            status = statuses[0];
+            i = 0;
+        }
+        client.user.setActivity(status)
+        i++;
+    }, 15000);
 })
