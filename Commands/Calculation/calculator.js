d81e459af1ca9f68370583c6c2103ef77d598cbb
451EK <cilmanemirkaan@gmail.com>
2021-09-27 12:23:46 +0300

    Small changes

--- Commands/Calculation/calculator.js
+++ Commands/Calculation/calculator.js
@@ -1,22 +1,14 @@
 const Discord = require('discord.js');
-const { Calculator } = require("weky");
+const simplydjs = require("simply-djs");
 
 module.exports = {
     name: "calculator",
     usage: "`-calculator`",
     description: "Displays a virtual calculator.",
     run: async(client,message) => {
-        Calculator({
-            message:message,
-            embed: {
-                title:"Virtual Calculator",
-                color: "#00ff94",
-                footer: "Calculator",
-                timestamp: true,
-            },
-            disabledQuery: "Calculator Disabled",
-            invalidQuery: 'The provided equation is invalid!',
-			othersMessage: 'Only <@{{author}}> can use the buttons!',
+        simplydjs.calculator(message, {
+            embedColor: "#00ff94",
+            credit: false,
         })
     }   
 }
